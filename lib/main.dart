d8b7c0adb1d5fad97060337ff62bcd33c287cdac
Felipe Sales <[soufeliposales@gmail.com]>
2020-11-05 12:07:41 -0300

    add effective dart and updates in codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
-  ));
-}
+));
+
 
 class Home extends StatefulWidget {
   @override
@@ -30,9 +29,9 @@ class _HomeState extends State<Home> {
 
   void _calculate() {
     setState(() {
-      double weight = double.parse(weightController.text);
-      double height = double.parse(heightController.text) / 100;
-      double imc = weight / (height * height);
+      var weight = double.parse(weightController.text);
+      var height = double.parse(heightController.text) / 100;
+      var imc = weight / (height * height);
       print(imc);
       if (imc < 18.6) {
         _infoText = 'Abaixo do Peso (${imc.toStringAsPrecision(4)})';
@@ -50,13 +49,13 @@ class _HomeState extends State<Home> {
     });
   }
 
-  static const _colorDefault = const Color(0xFF21e6c1);
-  static const _colorFields = const Color(0xFF393e46);
-  static const _colorBackground = const Color(0xFF222831);
-  static const _colorLight = const Color(0xFFe4f9ff);
+  static const _colorDefault    = Color(0xFF21e6c1);
+  static const _colorFields     = Color(0xFF393e46);
+  static const _colorBackground = Color(0xFF222831);
+  static const _colorLight      = Color(0xFFe4f9ff);
 
   final kLabelStyle = TextStyle(
-      color: _colorDefault, fontWeight: FontWeight.bold, fontSize: 26);
+    color: _colorDefault, fontWeight: FontWeight.bold, fontSize: 26);
 
   final kLightLabelStyle = TextStyle(color: _colorLight, fontSize: 20);
 
@@ -70,13 +69,13 @@ class _HomeState extends State<Home> {
     backgroundColor: _colorDefault,
     duration: Duration(seconds: 3),
     content: Text('Veja se um dos campos está vazio.',
-        style: TextStyle(color: _colorLight, fontSize: 18)),
+      style: TextStyle(color: _colorLight, fontSize: 18)),
   );
 
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  Widget _buildSizedBox() =>
-      Divider(color: _colorDefault, thickness: 1.5, indent: 75, endIndent: 75);
+  Widget _buildSizedBox() => Divider(
+    color: _colorDefault, thickness: 1.5, indent: 75, endIndent: 75);
 
   @override
   Widget build(BuildContext context) {
@@ -95,91 +94,85 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 50),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Icon(Icons.person, size: 100, color: _colorLight),
-                _buildSizedBox(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(height: 16),
-                    Text('Peso (kg)', style: kLabelStyle),
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                      decoration: kBoxDecorationStyle,
-                      child: TextFormField(
-                        style: kLightLabelStyle,
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                        controller: weightController,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            _scaffoldKey.currentState.showSnackBar(_snackBar);
-                          }
-                          return null;
-                        },
-                      ),
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person, size: 100, color: _colorLight),
+              _buildSizedBox(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 16),
+                  Text('Peso (kg)', style: kLabelStyle),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 80,vertical: 16),
+                    decoration: kBoxDecorationStyle,
+                    child: TextFormField(
+                      style: kLightLabelStyle,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(border: InputBorder.none),
+                      controller: weightController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          _scaffoldKey.currentState.showSnackBar(_snackBar);
+                        }
+                        return null;
+                      },
                     ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Altura (cm)', style: kLabelStyle),
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                      decoration: kBoxDecorationStyle,
-                      child: TextFormField(
-                        style: kLightLabelStyle,
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                        controller: heightController,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            _scaffoldKey.currentState.showSnackBar(_snackBar);
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                _buildSizedBox(),
-                SizedBox(height: 16),
-                Container(
-                  height: 50.0,
-                  child: RaisedButton(
-                    color: _colorFields,
-                    child: Text('Calcule', style: kLabelStyle),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50.0),
-                        side: BorderSide(width: 1.5, color: _colorDefault)),
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        _calculate();
-                      }
-                    },
                   ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Altura (cm)', style: kLabelStyle),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+                    decoration: kBoxDecorationStyle,
+                    child: TextFormField(
+                      style: kLightLabelStyle,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(border: InputBorder.none),
+                      controller: heightController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          _scaffoldKey.currentState.showSnackBar(_snackBar);
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              _buildSizedBox(),
+              SizedBox(height: 16),
+              Container(
+                height: 50.0,
+                child: RaisedButton(
+                  color: _colorFields,
+                  child: Text('Calcule', style: kLabelStyle),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                    side: BorderSide(width: 1.5, color: _colorDefault)),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _calculate();
+                    }
+                  },
                 ),
-                SizedBox(height: 24),
-                Text(_infoText,
-                    style: kLabelStyle, textAlign: TextAlign.center),
-              ],
-            ),
-          )),
+              ),
+              SizedBox(height: 24),
+              Text(_infoText, style: kLabelStyle, textAlign: TextAlign.center),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
